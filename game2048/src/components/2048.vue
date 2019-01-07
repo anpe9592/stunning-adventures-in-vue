dbbd75f8387592f249e6adf387626342390cf193
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-07 02:17:09 +0100

    added four colored squares
    red blue
    yellow green

--- game2048/src/components/2048.vue
+++ game2048/src/components/2048.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="game">
-    
+    <div class="red"></div>
+    <div class="blue"></div>
+    <div class="yellow"></div>
+    <div class="green"></div>
   </div>
 </template>
 
@@ -28,5 +31,55 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.game {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+
+.red {
+  float: left;
+  position: relative;
+  width: 35%;
+  padding-bottom: 30%;
+  margin: 1.66%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: #B20000;
+}
+
+.blue {
+  float: left;
+  position: relative;
+  width: 35%;
+  padding-bottom: 30%;
+  margin: 1.66%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: #2100B2;
+}
+
+.yellow {
+  float: left;
+  position: relative;
+  width: 35%;
+  padding-bottom: 30%;
+  margin: 1.66%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: #CCCC00;
+}
+
+.green {
+  float: left;
+  position: relative;
+  width: 35%;
+  padding-bottom: 30%;
+  margin: 1.66%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: #006400;
 }
 </style>
