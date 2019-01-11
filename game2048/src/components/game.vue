bc9c8404d4d749cd7fc635c854237a78811cbcdb
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-11 02:32:15 +0100

    added css
    they did not include the css code in the tutorial, had to look for it in the tutorials git repo

--- game2048/src/components/game.vue
+++ game2048/src/components/game.vue
@@ -64,5 +64,53 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.game {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
 
+.game-container {
+  display: flex;
+  justify-content: center;
+}
+
+.board {
+  display: flex;
+  flex-wrap: wrap;
+  width: 23em;
+  padding: 6px;
+  border-radius: 6px;
+  background-color: rgba(58, 41, 76, 0.5);
+}
+
+.shadow-board {
+  position: absolute;
+  height: 368px;
+  background-color: initial;
+}
+
+.tile {
+  border-radius: 6px;
+  box-sizing: border-box;
+  display: flex;
+  margin: 6px;
+  justify-content: center;
+  align-items: center;
+  color: #FEFDFB;
+  background-color: #91CCC8;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 5em;
+  height: 5em;
+}
+
+.shadow-tile {
+  z-index: -1;
+}
 </style>
