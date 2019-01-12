261aaa13aef3e79d3cb177c9817952b5a6f1204e
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-13 00:49:50 +0100

    added Vuex

---
+++ game2048/src/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vue.Store({
+  state: {
+    score: 0
+  },
+  mutations: {
+    updateScore (state, score) {
+      state.score += score
+    },
+    resetScore (state) {
+      state.score = 0
+    }
+  },
+  actions: {
+    updateScore (context, score) {
+      context.commit('updateScore', score)
+    },
+    resetScore (context) {
+      context.commit('resetScore')
+    }
+  }
+})
