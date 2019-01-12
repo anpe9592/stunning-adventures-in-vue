6cf31963255f88c587ae934871bbda9979d21d82
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-12 23:59:46 +0100

    created a component in the tile

---
+++ game2048/src/components/tile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tile">
+    {{ tile.value }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tile',
+  props: {
+    stile: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
