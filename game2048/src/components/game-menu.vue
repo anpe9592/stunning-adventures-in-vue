a699c5261ebd35f4ff6d9699c0e2abb1aad84ff4
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-14 23:44:15 +0100

    create a new file game-menu.vue with code copied from tile.vue

---
+++ game2048/src/components/game-menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="game-menu">
+    {{ tile.value }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'game-menu',
+  props: {
+    tile: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
