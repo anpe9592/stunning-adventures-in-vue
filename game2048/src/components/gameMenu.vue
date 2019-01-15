9afa0cab10a4038564e8a4371eeb7b93f2f55345
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-15 01:56:14 +0100

    add the css code for the menu
    score and new game button

--- game2048/src/components/gameMenu.vue
+++ game2048/src/components/gameMenu.vue
@@ -31,5 +31,70 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.gameMenu {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  width: 23em;
+  height: 7em;
+  padding: 1em;
+}
+
+.row {
+  display: flex;
+}
+
+.title {
+  font-size: 2.75em;
+  color: #1E1E12;
+}
+
+.scores {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1em;
+}
+
+.space-right {
+  margin-left: auto;
+}
+
+.score {
+  background-color: #91CCC8;
+  margin-left: 0.5em;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5em 1.5em;
+}
+
+.score-title {
+  font-size: .75em;
+}
+
+.score-value {}
+
+.button {
+  display: flex;
+  height: 2em;
+  justify-content: center;
+  padding: 0.1em 0.75em;
+  border: 1px solid #91CCC8;
+  color: #91CCC8;
+  border-radius: 6px;
+  align-items: center;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #91CCC8;
+  color: white;
+}
+
+.space-right {
+  margin-left: auto;
+}
 
 </style>
