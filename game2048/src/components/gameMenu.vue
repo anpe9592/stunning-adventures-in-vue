c6bf0c823f978df9ffd7d89e2506c1f77b057fb4
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-15 01:20:00 +0100

    added score and new game button

--- game2048/src/components/gameMenu.vue
+++ game2048/src/components/gameMenu.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="gameMenu">
-    {{ tile.value }}
+    <div class="row">
+      <div class="title">2048</div>
+      <div class="scores space-right">
+        <div class="score">
+          <div class="score-title">SCORE</div>
+          <div class="score-value">0000</div>
+        </div>
+        <div class="score">
+          <div class="score-title">BEST</div>
+          <div class="score-value">0000</div>
+        </div>
+      </div>
+    </div>
+    <a class="button space-right" @click="newGame()">New Game</a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'gameMenu',
-  props: {
-    tile: {
-      type: Object,
-      required: true
+  methods: {
+    newGame () {
+      this.$emit('new-game')
     }
   }
 }
