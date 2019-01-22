5d66ed7ef754880c2926be6bce5bb1f1e89e4071
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-01-22 12:10:14 +0100

    slide number to the right

--- game2048/src/mixins/control.js
+++ game2048/src/mixins/control.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export default {
   data () {
     return {
@@ -9,8 +11,74 @@ export default {
   },
 
   methods: {
+    animate () {
+      let boardDidChange = this.slideAnimationsList.length > 0
+
+      if (boardDidChange) {
+        while (this.slideAnimationsList.length > 0) {
+          let animation = this.slideAnimationsList.splice(0, 1)[0]
+          this.animateTiles(animation)
+        }
+        setTimeout(() => {
+          this.seedTwo()
+        }, 100)
+      }
+    },
+
+    animateTiles (animation) {
+      let { from, to } = animation
+
+      let a = this.board[from]
+      let b = this.board[to]
+      a.value = a.value + b.value
+      b.value = 0
+
+      this.board.splice(from, 1)
+      this.board.splice(from, 0, b)
+
+      this.board.splice(to, 1)
+      this.board.splice(to, 0, a)
+    },
+
+    getChangeLists () {
+      let changeLists = {}
+
+      changeLists.slide = this.slideAnimationsList
+
+      return changeLists
+    },
+
     moveRight () {
       console.log('key 39')
+      let changeLists = this.getChangeLists()
+
+      let board = _.cloneDeep(_.chunk(this.board, 4))
+      for (let a = 0; a < board.length; a++) {
+        this.slideRight(board, a, changeLists)
+      }
+    },
+
+    slideRight (board, a, changeLists) {
+      let k = board.length - 2
+      let l = board.length - 1
+      while (k >= 0) {
+        if (board[a][l].value !== 0) {
+          l--
+          k--
+        } else if (board[a][l].value !== 0 && board[a][k].value !== 0) {
+          l--
+          k--
+        } else if (board[a][l].value === 0 && board[a][k].value === 0) {
+          k--
+        } else if (board[a][l].value === 0 && board[a][k].value !== 0) {
+          changeLists.slide.push({ from: (a * 4 + k), to: (a * 4 + l) })
+
+          board[a][l].value = board[a][k].value + board[a][l].value
+          board[a][k].value = 0
+          l--
+          k--
+        }
+      }
     },
 
     moveLeft () {
@@ -29,7 +97,7 @@ export default {
       const validKeyCodes = [39, 37, 38, 40]
       document.addEventListener('keydown', (event) => {
         let key = event.which
-        if (validKeyCodes.includes(key)) {
+        if (_.includes(validKeyCodes, key)) {
           if (key === 39) {
             // right
             this.moveRight()
@@ -43,7 +111,8 @@ export default {
             // down
             this.moveDown()
           }
-        }
+          this.animate()
+        } else { return }
       })
     }
   }
