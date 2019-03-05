271df45bfcd33a9c8af979bfea4a86d44007a1d8
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-03-05 21:18:49 +0100

    changed the name of the two files

--- tic-tac-toe/tic-tac-toe/src/components/Game.vue
+++ tic-tac-toe/tic-tac-toe/src/components/Game.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Game',
   props: {
     msg: String
   }
--- tic-tac-toe/tic-tac-toe/src/router.js
+++ tic-tac-toe/tic-tac-toe/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/TicTacToe.vue'
 
 Vue.use(Router)
 
