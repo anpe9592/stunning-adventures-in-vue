4097c8ab80a9795adabce541919f84cf82daecb3
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-03-14 18:13:30 +0100

    seed a random 2 on tap

--- tic-tac-toe/src/mixins/control.js
+++ tic-tac-toe/src/mixins/control.js
@@ -2,9 +2,32 @@
 
 export default {
   methods: {
+    tap (a) {
+      console.log(a)
+      this.seedTwo()
+    },
+
+    seedTwo () {
+      if (this.allTilesFull) { return }
+
+      let getRandomItem = () => {
+        let randomIndex = Math.floor(Math.random() * this.board.length)
+
+        return this.board[randomIndex]
+      }
+
+      let randomItem = getRandomItem()
+
+      while (randomItem.value !== 0) {
+        randomItem = getRandomItem()
+      }
+
+      randomItem.value = 2
+    },
+
     onTap (event) {
       if (event.isFinal) {
-        console.log(event)
+        this.tap(event)
       }
     }
   }
