80f33e44f1a501165692e95412886d7b3f495af0
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-03-26 17:22:19 +0100

    removed unused code

--- tic-tac-toe/src/mixins/control.js
+++ tic-tac-toe/src/mixins/control.js
@@ -1,43 +1,9 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
 export default {
   methods: {
     playerAy () {
       console.log('hello')
-      for (let a = 0; a < board.length; a++) {
-        this.dosomthing(board, a, x)
-      }
-      this.seedTwo()
-    },
-
-    dosomthing (board, a, x) {
-      console.log('x', x)
-      console.log('a', a)
-      console.log('board', board[0][0])
-    },
-
-    seedTwo () {
-      if (this.allTilesFull) { return }
-
-      let getRandomItem = () => {
-        let randomIndex = Math.floor(Math.random() * this.board.length)
-
-        return this.board[randomIndex]
-      }
-
-      let randomItem = getRandomItem()
-
-      while (randomItem.value !== 0) {
-        randomItem = getRandomItem()
-      }
-
-      randomItem.value = 2
-    },
-
-    onTap (event) {
-      if (event.isFinal) {
-        this.tap(event.center)
-      }
     }
   }
 }
