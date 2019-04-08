22d7e763499e69a4815d53c89590de10f1e3720f
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-04-08 22:57:53 +0200

    check the columns if the player won

--- tic-tac-toe/src/components/Game.vue
+++ tic-tac-toe/src/components/Game.vue
@@ -81,6 +81,16 @@ export default {
       if (this.board[6].value === 2 && this.board[7].value === 2 && this.board[8].value === 2) {
         this.gameOver = true
       }
+      // col
+      if (this.board[0].value === 2 && this.board[3].value === 2 && this.board[6].value === 2) {
+        this.gameOver = true
+      }
+      if (this.board[1].value === 2 && this.board[4].value === 2 && this.board[7].value === 2) {
+        this.gameOver = true
+      }
+      if (this.board[2].value === 2 && this.board[5].value === 2 && this.board[8].value === 2) {
+        this.gameOver = true
+      }
     },
 
     checkGameState () {
