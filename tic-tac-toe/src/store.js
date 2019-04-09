8d773069bdbcc71bf6042e036333918aedcbd109
Andreas Petersson <anpe9592@users.noreply.github.com>
2019-04-09 23:26:14 +0200

    store who is the winner

--- tic-tac-toe/src/store.js
+++ tic-tac-toe/src/store.js
@@ -6,15 +6,22 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     score: 0,
-    best: 0
+    best: 0,
+    winner: 0
 
   },
   mutations: {
+    updateWinner (state, winner) {
+      state.winner = winner
+    },
     updateScore (state, score) {
       state.score += score
     },
     updateBest (state, best) {
       state.best = best
+    },
+    resetWinner (state) {
+      state.winner = 0
     },
     resetScore (state) {
       state.score = 0
@@ -24,11 +31,17 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    updateWinner (context, winner) {
+      context.commit('updateWinner', winner)
+    },
     updateScore (context, score) {
       context.commit('updateScore', score)
     },
     updateBest (context, best) {
       context.commit('updateBest', best)
+    },
+    resetWinner (context) {
+      context.commit('resetWinner')
     },
     resetScore (context) {
       context.commit('resetScore')
